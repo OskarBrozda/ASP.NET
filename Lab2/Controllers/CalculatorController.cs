149a03037ed62531d83f2c86f991c3874a8ce9e8
Oskar Brózda <oskarbrozdaa@gmail.com>
2023-10-10 14:58:01 +0200

    lab2 task2 in progress

---
+++ Lab2/Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using Lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+/*
+1. użytkownik klika na link w menu aplikacji prowadzący do formularza, który kieruje do metody akcji Form w klasie CalculatorCopntroller.
+2. Metoda Form zwraca widok formularza.
+3. Po wypełnieniu formularza użytkownik go zatwierdza przyciskiem Oblicz, który generuje żądanie zawierające dane z formularza.
+4. Żądanie trafia do metody akcji Result kontrolera CalculatorController
+5. metoda Result wykonuje obliczenia na podstawie danych i zwraca widok z wynikami obliczeń
+ */
+
+namespace Lab2.Controllers
+{
+    public class CalculatorController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Result([FromForm] Calculator model)
+        {
+            if (!model.IsValid())
+            {
+                return View("Error");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Lab2/Models/Birth.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Models
+{
+    public class Birth : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
