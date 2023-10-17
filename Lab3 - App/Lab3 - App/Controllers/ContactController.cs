15f3da5d8ed6d037ed082973ac215b7f06db7cb6
Oskar Brózda <oskarbrozdaa@gmail.com>
2023-10-17 15:03:05 +0200

    Create App (delete, details, add, edit)

---
+++ Lab3 - App/Lab3 - App/Controllers/ContactController.cs
@@ -0,0 +1,50 @@
+﻿using Lab3___App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3___App.Controllers
+{
+    public class ContactController : Controller
+    {
+        static int index = 1;
+        static readonly Dictionary<int, Contact> _contacts = new Dictionary<int, Contact>();
+        public IActionResult Index()
+        {
+            return View(_contacts);
+        }
+
+        [HttpGet] // wyświetla formularz
+        public IActionResult Create() => View();
+
+        [HttpPost] // pobiera dane z formularza
+        public IActionResult Create(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Id = index++;
+                _contacts[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id) => View(_contacts[id]);
+
+        [HttpPost]
+        public IActionResult Update(Contact model)
+        {
+            if(ModelState.IsValid)
+            {
+                _contacts[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id) => View(_contacts[id]);
+
+        [HttpGet]
+        public IActionResult Delete(int id) => View(_contacts[id]);
+    }
+}
---
+++ Lab3 - App/Lab3 - App/Models/Contact.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Constraints;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3___App.Models
+{
+    public class Contact
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Proszę podać imię.")]
+        [StringLength(maximumLength:100, MinimumLength = 3, ErrorMessage = "Długość imienia powinna mieścić sie między 3, a 100 znaków.")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = " Proszę podać email.")]
+        [EmailAddress(ErrorMessage = "Niepoprawny adres email")]
+        public string Email { get; set; }
+
+
+        [Phone(ErrorMessage = "Niepoprawny numer telefonu.")]
+        public string? Phone { get; set; }
+
+
+        public DateTime? Birth { get; set; }
+    }
+}
